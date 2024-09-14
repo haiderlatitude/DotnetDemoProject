e7a1808e991976002f238154a7fbcfc03dd61e6b
Haider Ali <wahabw259@gmail.com>
2024-09-15 04:14:11 +0500

    initialization

---
+++ Models/Expense.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DemoProject.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public decimal Value { get; set; }
+    
+    [Required]
+    public string? Description { get; set; }
+}
